2bd710ddab5aaf196b3e997cba3557618a29ecd2
Rudra23379 <23379@students.mrgs.school.nz>
2025-07-27 23:27:21 +1200

    home

--- java.js
+++ java.js
@@ -5,7 +5,6 @@ function openNav() {
   document.getElementById("main").style.marginLeft = "250px";
 }
 
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
